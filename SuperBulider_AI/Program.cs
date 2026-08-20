6b6465131ac299dee1f6da0322cb845b67d11bd1
wenzhong0606-dev <wenzhong0606@gmail.com>
2026-08-20 10:47:02 +0800

    Phase 2.6 C.9 register golden baseline release service

--- SuperBulider_AI/Program.cs
+++ SuperBulider_AI/Program.cs
@@ -54,6 +54,7 @@ builder.Services.AddScoped<GoldenConfidenceCalibrationEvaluator>();
 builder.Services.AddScoped<GoldenConfidenceCalibrationRunner>();
 builder.Services.AddScoped<GoldenDatasetCoverageAnalyzer>();
 builder.Services.AddScoped<GoldenDatasetQualityGate>();
+builder.Services.AddScoped<GoldenBaselineReleaseService>();
 builder.Services.AddScoped<GoldenDatasetRunner>();
 builder.Services.AddScoped<GoldenDatasetRegressionEvaluator>();
 builder.Services.AddScoped<IDataSourceConnectionFactory, DataSourceConnectionFactory>();
