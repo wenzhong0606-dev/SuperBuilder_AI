af2254747ca042964794028bb3eceb70e01daf6a
wenzhong0606-dev <wenzhong0606@gmail.com>
2026-08-20 09:25:09 +0800

    Phase 2.6 C.4.4 add Table evaluation result section

--- SuperBulider_AI/Models/BI/Evaluation/QueryPlanEvaluationResult.cs
+++ SuperBulider_AI/Models/BI/Evaluation/QueryPlanEvaluationResult.cs
@@ -12,6 +12,7 @@ public sealed class QueryPlanEvaluationResult
     public QueryPlanEvaluationSectionResult Metrics { get; init; } = new();
     public QueryPlanEvaluationSectionResult Dimensions { get; init; } = new();
     public QueryPlanEvaluationSectionResult Filters { get; init; } = new();
+    public QueryPlanEvaluationSectionResult Tables { get; init; } = new();
     public QueryPlanEvaluationSectionResult QueryShape { get; init; } = new();
     public QueryPlanEvaluationSectionResult BindingConsistency { get; init; } = new();
 }
