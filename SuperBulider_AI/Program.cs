e685f2ae59f0d7482e57a9dccb1530a9bfd1d984
wenzhong0606-dev <wenzhong0606@gmail.com>
2026-08-25 14:12:25 +0800

    D14修复运行时DI：注册DimensionResolutionEvidenceService

--- SuperBulider_AI/Program.cs
+++ SuperBulider_AI/Program.cs
@@ -47,6 +47,7 @@ builder.Services.AddScoped<MetadataCsvFixtureService>();
 builder.Services.AddScoped<IMetadataCsvFixtureService>(sp => sp.GetRequiredService<MetadataCsvFixtureService>());
 builder.Services.AddScoped<MetadataSemanticSearchService>();
 builder.Services.AddScoped<IMetadataSemanticSearchService>(sp => sp.GetRequiredService<MetadataSemanticSearchService>());
+builder.Services.AddScoped<IDimensionResolutionEvidenceService, DimensionResolutionEvidenceService>();
 builder.Services.AddScoped<QueryIntentNormalizer>();
 builder.Services.AddScoped<QueryUnderstandingService>();
 builder.Services.AddScoped<IQueryUnderstandingService>(sp => sp.GetRequiredService<QueryUnderstandingService>());
